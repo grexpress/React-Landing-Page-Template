581c582485bfca78cf807174f1b74250075186d0
grexpress <79295315+grexpress@users.noreply.github.com>
2021-02-20 09:05:03 +0700

    update-functions

--- functions/send-email.js
+++ functions/send-email.js
@@ -3,41 +3,47 @@ const nodemailer = require('nodemailer')
 const {EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS, EMAIL_RECIPIENT} = process.env
 
 exports.handler = async (event, context) => {
-    const transporter = nodemailer.createTransport({
-        host: EMAIL_HOST || 'smtp.gmail.com',
-        port: EMAIL_PORT || 587,
-        secure: true,
-        auth: {
-            user: EMAIL_USER,
-            pass: EMAIL_PASS,
-        },
-    })
-
     const result = await parser.parse(event)
-    const {name, email, phone, position, files} = result
-
-    const subject = `[${position}] ${name}`
-    const text = `
-        Name: ${name}\n
-        Applying for position: ${position}\n
-        Phone: ${phone}\n
-        Email: ${email}\n
-        CV: find attachment
-      `
-    let info = await transporter.sendMail({
-        from: email,
-        to: EMAIL_RECIPIENT,
-        subject,
-        text,
-        attachments: files,
-    })
-
-    console.log("")
-
-    if (info) {
-        return {
-            statusCode: 200,
-            body: 'Ok',
-        }
+    return {
+        statusCode: 200,
+        body: 'Ok',
+        formData: result
     }
+
+    // const transporter = nodemailer.createTransport({
+    //     host: EMAIL_HOST || 'smtp.gmail.com',
+    //     port: EMAIL_PORT || 587,
+    //     secure: true,
+    //     auth: {
+    //         user: EMAIL_USER,
+    //         pass: EMAIL_PASS,
+    //     },
+    // })
+    //
+    // const {name, email, phone, position, files} = result
+    //
+    // const subject = `[${position}] ${name}`
+    // const text = `
+    //     Name: ${name}\n
+    //     Applying for position: ${position}\n
+    //     Phone: ${phone}\n
+    //     Email: ${email}\n
+    //     CV: find attachment
+    //   `
+    // let info = await transporter.sendMail({
+    //     from: email,
+    //     to: EMAIL_RECIPIENT,
+    //     subject,
+    //     text,
+    //     attachments: files,
+    // })
+    //
+    // console.log("")
+    //
+    // if (info) {
+    //     return {
+    //         statusCode: 200,
+    //         body: 'Ok',
+    //     }
+    // }
 }
