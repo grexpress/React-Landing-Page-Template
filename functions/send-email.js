1f70e379e66cd17820a5c756df3d077ced82a295
grexpress <79295315+grexpress@users.noreply.github.com>
2021-02-20 09:17:08 +0700

    update-functions

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,49 +1,58 @@
 const parser = require('lambda-multipart-parser')
 const nodemailer = require('nodemailer')
-const {EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS, EMAIL_RECIPIENT} = process.env
+let {EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS, EMAIL_RECIPIENT} = process.env
 
 exports.handler = async (event, context) => {
-    const result = await parser.parse(event)
-    return {
-        statusCode: 200,
-        body: 'Ok',
-        formData: result
+    const data = await parser.parse(event)
+    return sendEmail(data)
+}
+
+async function sendEmail({name, email, phone, position, files} = {}, test = false) {
+    if(test) {
+        EMAIL_USER = "thanhnv.sed@gmail.com"
+        EMAIL_PASS = "ctkvrvrdqidobykk"
+        EMAIL_RECIPIENT = "thanhnv.sed@gmail.com"
     }
 
-    // const transporter = nodemailer.createTransport({
-    //     host: EMAIL_HOST || 'smtp.gmail.com',
-    //     port: EMAIL_PORT || 587,
-    //     secure: true,
-    //     auth: {
-    //         user: EMAIL_USER,
-    //         pass: EMAIL_PASS,
-    //     },
-    // })
-    //
-    // const {name, email, phone, position, files} = result
-    //
-    // const subject = `[${position}] ${name}`
-    // const text = `
-    //     Name: ${name}\n
-    //     Applying for position: ${position}\n
-    //     Phone: ${phone}\n
-    //     Email: ${email}\n
-    //     CV: find attachment
-    //   `
-    // let info = await transporter.sendMail({
-    //     from: email,
-    //     to: EMAIL_RECIPIENT,
-    //     subject,
-    //     text,
-    //     attachments: files,
-    // })
-    //
-    // console.log("")
-    //
-    // if (info) {
-    //     return {
-    //         statusCode: 200,
-    //         body: 'Ok',
-    //     }
-    // }
+    const transporter = nodemailer.createTransport({
+        host: EMAIL_HOST || 'smtp.gmail.com',
+        port: EMAIL_PORT || 587,
+        secure: false,
+        auth: {
+            user: EMAIL_USER,
+            pass: EMAIL_PASS,
+        },
+    })
+
+    const subject = `[${position}] ${name}`
+    const text = `
+        Name: ${name}\n
+        Applying for position: ${position}\n
+        Phone: ${phone}\n
+        Email: ${email}\n
+        CV: find attachment
+      `
+    let info = await transporter.sendMail({
+        from: email,
+        to: EMAIL_RECIPIENT,
+        subject,
+        text,
+        attachments: files,
+    })
+
+    console.log("send email info: ", info)
+    if (info && (!info.rejected || info.rejected.length == 0)) {
+        return {
+            statusCode: 200,
+            body: { success: true, details: info },
+        }
+    }
 }
+
+// sendEmail({
+//     name: "Thanh",
+//     email: "thanhnv.sed@gmail.com",
+//     phone: "2349028394",
+//     position: "Dev",
+//     files: []
+// }, true).catch(e => console.log(e))
