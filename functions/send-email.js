69487ffc4d89ca07b948c6c57708975230a04e80
grexpress <79295315+grexpress@users.noreply.github.com>
2021-02-20 09:31:59 +0700

    Update send-email.js

--- functions/send-email.js
+++ functions/send-email.js
@@ -2,16 +2,16 @@ const parser = require('lambda-multipart-parser')
 const nodemailer = require('nodemailer')
 let {EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS, EMAIL_RECIPIENT} = process.env
 
-exports.handler = async (event, context) => {
-    const data = await parser.parse(event)
-    return {
+exports.handler = (event, context, callback) => {
+    const data = parser.parse(event)
+    callback(null,  {
         statusCode: 200,
         body: {
             success: true,
             data,
             env: { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS, EMAIL_RECIPIENT }
         },
-    }
+    });
     // return sendEmail(data)
 }
 
